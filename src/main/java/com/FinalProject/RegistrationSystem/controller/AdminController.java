1fb287e91d17e903bca478092529e17af727cc88
isaac-adesanya <adesanyatosin07@gmail.com>
2026-03-20 18:21:31 -0600

    Implemented admin endpoints

--- src/main/java/com/FinalProject/RegistrationSystem/controller/AdminController.java
+++ src/main/java/com/FinalProject/RegistrationSystem/controller/AdminController.java
@@ -1,4 +1,43 @@
 package com.FinalProject.RegistrationSystem.controller;
 
+import com.FinalProject.RegistrationSystem.dto.CreateWorkshopRequest;
+import com.FinalProject.RegistrationSystem.model.Registration;
+import com.FinalProject.RegistrationSystem.model.Workshop;
+import com.FinalProject.RegistrationSystem.service.RegistrationService;
+import com.FinalProject.RegistrationSystem.service.WorkshopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin/workshops")
 public class AdminController {
+
+    @Autowired
+    private WorkshopService workshopService;
+
+    @Autowired
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public Workshop create(@RequestBody CreateWorkshopRequest request) {
+        return workshopService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public Workshop update(@PathVariable Long id, @RequestBody Workshop updated) {
+        return workshopService.update(id, updated);
+    }
+
+    @PatchMapping("/{id}/cancel")
+    public void cancel(@PathVariable Long id) {
+        workshopService.cancelWorkshop(id);
+    }
+
+    @GetMapping("/{id}/registrations")
+    public List<Registration> getRegistrations(@PathVariable Long id) {
+        return registrationService.getAllWorkshopRegistrations(id);
+    }
 }
+
