a8a6d8261928b3f9e7700462a660f7514bae0915
isaac-adesanya <adesanyatosin07@gmail.com>
2026-04-17 19:35:26 -0600

    implemented bean validation and error handling

--- src/main/java/com/FinalProject/RegistrationSystem/dto/CreateUserRequest.java
+++ src/main/java/com/FinalProject/RegistrationSystem/dto/CreateUserRequest.java
@@ -1,9 +1,16 @@
 package com.FinalProject.RegistrationSystem.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 
 public class CreateUserRequest {
     public String name;
+    @Valid
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", message = "Email is invalid")
     public String email;
+    @Valid
+    @Min(value = 8, message = "Password must be at least 8 characters")
     public String password_hash;
 }
--- src/main/java/com/FinalProject/RegistrationSystem/service/WorkshopService.java
+++ src/main/java/com/FinalProject/RegistrationSystem/service/WorkshopService.java
@@ -7,11 +7,13 @@ import com.FinalProject.RegistrationSystem.repository.WorkshopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Validated
 public class WorkshopService {
 
     @Autowired
